15a7df5ebffb192463e33daf6894859b19fb9777
Andrew Stepanov <stepanov.management@gmail.com>
2023-09-28 10:39:58 +0500

    Описаны все типы

---
+++ src/shared/types/index.ts
@@ -0,0 +1,6 @@
+export * from './cities.enum.js';
+export * from './comment.type.js';
+export * from './facilities.enum.js';
+export * from './offer.type.js';
+export * from './property.enum.js';
+export * from './user.type.js';
---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,27 @@
+import { Cities } from './cities.enum.js';
+import { PropertyType } from './property.enum.js';
+import { Facilities } from './facilities.enum.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  publishedDate: Date;
+  city: Cities;
+  preview: string;
+  photos: string[];
+  premium: boolean;
+  favorite: boolean;
+  rating: number;
+  type: PropertyType;
+  roomsCount: number;
+  guestsCount: number;
+  price: number;
+  convienience: Facilities;
+  author: User;
+  commentsCount: number;
+  location: {
+    latitude: number,
+    longitude: number
+  };
+}
