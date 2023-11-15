b075b8e95f2bd3dc8aae0debea110c2ae65191d8
Andrew Stepanov <stepanov.management@gmail.com>
2023-11-15 20:04:52 +0500

    Сделал расчет рейтингов

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -141,7 +141,7 @@ export default class OfferController extends BaseController {
   public async getPremium({ params, tokenPayload }: Request, res: Response): Promise<void> {
     const { city } = params;
     const cityName = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
-    let offers;
+    let offers = [];
     if (tokenPayload) {
       offers = await this.offerService.findPremium(cityName, tokenPayload.id);
     } else {
@@ -159,7 +159,18 @@ export default class OfferController extends BaseController {
 
   public async getFavorites({ tokenPayload }: Request, res: Response): Promise<void> {
     const offers = await this.offerService.findUserFavorites(tokenPayload.id);
-    this.ok(res, fillDTO(OfferRdo, offers));
+
+    if (offers) {
+      const offersWithRatings = await Promise.all(offers.map(async (offer) => {
+        const preRating = await this.offerService.calculateRating(offer._id.toString());
+        const rating = preRating === null ? 0 : preRating;
+        return { ...offer, rating };
+      }));
+
+      this.ok(res, fillDTO(OfferRdo, offersWithRatings));
+    } else {
+      this.ok(res, fillDTO(OfferRdo, []));
+    }
   }
 
   public async updateFavoriteStatus({ params: { offerId, status }, tokenPayload }: Request, res: Response): Promise<void> {
@@ -168,10 +179,12 @@ export default class OfferController extends BaseController {
     }
     await this.offerService.changeFavoriteStatus(tokenPayload.id, offerId as string, parseInt(status, 10));
     const offer = await this.offerService.findById(offerId as string);
+    const preRating = await this.offerService.calculateRating(offerId);
+    const rating = preRating === null ? 0 : preRating;
     if (!offer) {
       throw new HttpError(StatusCodes.NOT_FOUND, 'Offer not found', '');
     }
-    this.ok(res, fillDTO(OfferRdo, {...offer.toJSON(), isFavorite: Boolean(Number(status))}));
+    this.ok(res, fillDTO(OfferRdo, {...offer.toJSON(), isFavorite: Boolean(Number(status)), rating}));
   }
 
   public async create({ body, tokenPayload }: CreateOfferRequest, res: Response): Promise<void> {
@@ -196,7 +209,11 @@ export default class OfferController extends BaseController {
 
   public async update({ body, params, tokenPayload }: Request<ParamOfferId, unknown, UpdateOfferDto>, res: Response): Promise<void> {
     const updatedOffer = await this.offerService.updateById(body, params.offerId, tokenPayload.id);
-    this.ok(res, fillDTO(FullOfferRdo, updatedOffer));
+    if (updatedOffer) {
+      const preRating = await this.offerService.calculateRating(params.offerId);
+      const rating = preRating === null ? 0 : preRating;
+      this.ok(res, fillDTO(FullOfferRdo, {...updatedOffer.toJSON(), rating}));
+    }
   }
 
   public async getComments({ params }: Request<ParamOfferId>, res: Response): Promise<void> {
